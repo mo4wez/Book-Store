a82e44b5dcd04ae209c13070a723a993d18ec7e0
moawezz <moawez.de@gmail.com>
2023-08-21 13:45:55 +0330

    Create pages app and crispy-forms

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party apps
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     # local apps
     'accounts',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# crispy forms setting
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'

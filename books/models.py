9d1e61839abd419b56f9f2a7155a490159bb4b07
moawezz <moawez.de@gmail.com>
2023-08-22 17:02:59 +0330

    Add Comment model to book

--- books/models.py
+++ books/models.py
@@ -15,3 +15,15 @@ class Book(models.Model):
     
     def get_absolute_url(self):
         return reverse("book_detail", kwargs={"pk": self.pk})
+
+class Comment(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, related_name="comments", on_delete=models.CASCADE)
+    text = models.TextField()
+    is_active = models.BooleanField(default=True)
+    recommended = models.BooleanField(default=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text

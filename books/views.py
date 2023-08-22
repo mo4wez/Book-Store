d6f2a093cdb4c8021fccb147e0a5c3682513bddf
moawezz <moawez.de@gmail.com>
2023-08-22 12:41:45 +0330

    Add Book list,detail,Create,update,delete views and Templates

--- books/views.py
+++ books/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Book
+
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'books/books_list.html'
+    context_object_name = 'books'
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book'
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price',]
+    template_name = 'books/book_create.html'
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price',]
+    template_name = 'books/book_update.html'
+
+class BookDeleteView(generic.DeleteView):
+    model = Book
+    template_name = 'books/book_delete.html'
+    success_url = reverse_lazy('books_list')
